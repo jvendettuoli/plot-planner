caefdb29c60c0f82afc499e39ff8c99b5e4538ba
Justin Vendettuoli <jvendettuoli@gmail.com>
2020-09-09 14:05:47 -0700

    removed caching-off settings in Flask

--- app.py
+++ app.py
@@ -1242,12 +1242,12 @@ logging.debug("End of app.py")
 # https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
 
 
-@app.after_request
-def add_header(req):
-    """Add non-caching headers on every request."""
+# @app.after_request
+# def add_header(req):
+#     """Add non-caching headers on every request."""
 
-    req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    req.headers["Pragma"] = "no-cache"
-    req.headers["Expires"] = "0"
-    req.headers["Cache-Control"] = "public, max-age=0"
-    return req
+#     req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     req.headers["Pragma"] = "no-cache"
+#     req.headers["Expires"] = "0"
+#     req.headers["Cache-Control"] = "public, max-age=0"
+#     return req
